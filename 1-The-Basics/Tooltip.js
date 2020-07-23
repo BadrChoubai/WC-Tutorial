ade154c96e41073ce69639375a642b0aec630a08
Badr Choubai <badrchoubai@outlook.com>
2020-07-22 18:09:42 -0600

    add event listeners

--- 1-The-Basics/Tooltip.js
+++ 1-The-Basics/Tooltip.js
@@ -1,12 +1,28 @@
 class ToolTip extends HTMLElement {
   constructor() {
     super();
+    this._tooltipContainer;
   }
 
   connectedCallback() {
-    const toolTipIcon = document.createElement("i");
-    toolTipIcon.classList.add("fa", "fa-question");
-    this.appendChild(toolTipIcon);
+    const tooltipIcon = document.createElement("i");
+    tooltipIcon.classList.add("fa", "fa-question");
+    tooltipIcon.style.cursor = "pointer";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+
+    this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer.textContent = "toolTip Text";
+
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
   }
 }
 
